c21f2c170ef4b04dbc6d26c83b473e8e8548ee1a
Stav Kidron <kidronstav@gmail.com>
2020-10-11 20:48:00 -0500

    added operator key

--- Server/server.js
+++ Server/server.js
@@ -29,3 +29,95 @@ app.post('/total', (req, res) => {
 app.listen(port, () => { //listen keeps server open
     console.log('Up and running on port', port); //message confirming that server is running   
 });
+
+
+///////////////////////////////////////////////////////////
+// LOGIC //
+///////////////////////////////////////////////////////////
+
+function execute(){
+    $('#addBtn').on('click', add);
+    $('#subtractBtn').on('click', subtract);
+    $('#multiplyBtn').on('click', multiply);
+    $('#divideBtn').on('click', divide);
+}
+
+
+function equateTotal(){
+
+    // if nothing inputed, log "no numbers inputed"
+    if($('#firstValue').val() === '' && $('#secondValue').val() === '') {
+        console.log('No numbers inputed');
+        return;
+    }
+    
+    // if add button = checked, add numbers
+    else if($('#addBtn').is(':checked')){
+        total = add();
+        operator = "+";
+        return total;
+    }
+    
+    // if subtract button = checked, subtract number
+    else if($('#subtractBtn').is(':checked')){
+        total = subtract();
+        console.log(total);
+        // clear inputs after hitting submit button
+        $('#firstValue').val('');
+        $('#secondValue').val('');
+        return total;
+    }
+    
+    // if multiply button = checked, multiply numbers
+    else if($('#multiplyBtn').is(':checked')){
+        let total = multiply();
+        console.log(total);
+        // clear inputs after hitting submit button
+        $('#firstValue').val('');
+        $('#secondValue').val('');
+        return total;
+    }
+    
+    // if divide button = checked, divide numbers
+    else if($('#divideBtn').is(':checked')){
+        let total = divide();
+        console.log(total);
+        // clear inputs after hitting submit button
+        $('#firstValue').val('');
+        $('#secondValue').val('');
+        return total;
+    }
+}
+
+// function clearCalc(){ // sets the number inputs to an empty string
+//     $('#firstValue').val(''),
+//     $('#secondValue').val('')
+// }
+
+function add(){
+    if($('#addBtn').is(':checked')){
+        let total = Number($('#firstValue').val()) + Number($('#secondValue').val());
+        return total;
+    }
+}
+
+function subtract(){
+    if($('#subtractBtn').is(':checked')){
+        let total = Number($('#firstValue').val()) - Number($('#secondValue').val());
+        return total;
+    }
+}
+
+function multiply(){
+    if($('#multiplyBtn').is(':checked')){
+        let total = Number($('#firstValue').val()) * Number($('#secondValue').val());
+        return total;
+    }
+}
+
+function divide(){
+    if($('#divideBtn').is(':checked')){
+        let total = Number($('#firstValue').val()) / Number($('#secondValue').val());
+        return total;
+    }
+}
